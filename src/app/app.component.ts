8d3fd1bf0d1bf3defb9a4f8ccf78de7eba69b976
fontgalland <44510192+fontgalland@users.noreply.github.com>
2023-07-12 11:51:11 -0300

    Services-tips-and-usage

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { AccountService } from './shared/account.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  accounts: {name: string, status:string}[] = [];
+
+  constructor(private accountsService: AccountService) {}
+
+  ngOnInit() {
+    this.accounts = this.accountsService.accounts;
+  }
+}
